ff6e3716016435408c083603e6cacc23b5a519fa
Maiorovaek <katushkm@yandex.ru>
2023-08-02 19:53:59 +0300

    add comments

--- ImageProxy/src/main/java/virtualbox/ImageProxy.java
+++ ImageProxy/src/main/java/virtualbox/ImageProxy.java
@@ -40,12 +40,19 @@ public class ImageProxy implements Icon {
             imageIcon.paintIcon(c,g,x,y);
         } else {
             g.drawString("Loading Pictures, please wait...", x+300, y+190);
+            //если загрузка изображения не началась
             if(!retrieving){
                 retrieving = true;
                 retrievalThread = new Thread(new Runnable() {
                     public void run() {
+                        //загружается Настоящее изображение.
+                        // Загрузка синхронна: конструктор ImageIcon не возвращает управление до завершения загрузки.
+                        // Операция обновления экрана и вывод сообщения  происходит Асинхронно.
                         try{
+                            // создаем отдельный экземпляр объекта Icon,
+                            // Его конструктор не возвращает управление до завершения загрузки данных.
                             setImageIcon(new ImageIcon(imageURL, "CD cover"));
+                            //оповещвем swing  о перерисовке
                             c.repaint();
                         } catch (Exception e){
                             e.printStackTrace();
